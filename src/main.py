65630e0b42e0ba6dacda3c0f2c420e34bbd2c265
Maria Paula Almeida De Freitas <mariaalm@c1r2s1.42porto.com>
2026-04-30 19:36:18 +0100

    update the files

---
+++ src/main.py
@@ -0,0 +1,10 @@
+#mapa = MapParser("mapa.txt").parse()
+#simulacao = Simulation(mapa)
+
+#if args.visual:
+    # Só aqui é que o Pygame entra em ação
+ #   view = Visualizer(mapa)
+  #  view.run(simulacao)
+#else:
+    # Modo terminal padrão (obrigatório)
+ #   simulacao.run_text_mode()
--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass, field
 from typing import Optional, Dict, List
 from models import Zone, Connection
 
@@ -14,13 +13,30 @@ class MapParser:
     
     def parse(self) -> None:
         try:
-            with open(self.file_path, 'r', enconding='utf-8') as f:
+            with open(self.file_path, 'r', encoding='utf-8') as f:
                 content = f.read()
             lines = content.splitlines()
+            # encontrar quantidade de drones
             for line in lines:
                 clean = line.split('#')[0].strip()
                 if clean.startswith("drone_count:"):
-                    
+                    try:
+                        self.drone_count = int(clean.split(':')[1].strip())
+                        break
+                    except ValueError:
+                        raise ValueError("Invalid drone_count format.")
+            if self.drone_count <= 0:
+                raise ValueError("Drone_count missing or invalid")
+            # processa zonas e conexoes
+            for line_num, line in enumerate(lines, 1):
+                clean = line.split('#')[0].strip()
+                if not clean or clean.startswith("drone_count:"):
+                    continue
         except Exception as error:
             print(f"Error to open the map: {error}")
             exit(1)
+    
+    def _parse_line(self, line: str, line_num: int) -> None:
+        # identifica se a linha é uma Zona ou uma Conexão.
+
+        
