30d5c4a3cc4fef1a7fe69d8c3409a205826d5769
Maria Paula Almeida De Freitas <mariaalm@c1r2s7.42porto.com>
2026-05-01 15:06:22 +0100

    update

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,6 @@
 from typing import Optional, Dict, List
 from models import Zone, Connection
+import re
 
 
 class MapParser:
@@ -35,8 +36,35 @@ class MapParser:
         except Exception as error:
             print(f"Error to open the map: {error}")
             exit(1)
+        self._validate_map() # fazer validate_map
     
     def _parse_line(self, line: str, line_num: int) -> None:
         # identifica se a linha é uma Zona ou uma Conexão.
+        metadata_filter = re.search(r'\[(.*?)\]', line)
+        metadata_str = metadata_filter.group(1) if metadata_filter else ""
+        metadata_dict = self._parse_metadata(metadata_str) # fazer parse_metadata
+        base_line = re.sub(r'\s*\[.*?\]\s*', '', line).strip()
+        if (base_line.startswith("start_hub:")
+            or base_line.startswith("end_hub:")
+            or base_line.startswith("hub:")):
+            self._parse_zone(base_line, metadata_dict, line_num) #fazer parse_zone
+        elif base_line.startswith("connection:"):
+            self._parse_connection(base_line, metadata_dict, line_num) # fazer parse_connection
+        else:
+            raise ValueError(f"Unknown syntax on line: {line_num}: {line}")
+        
+    def _parse_metadata(self, metadata_str: str) -> Dict[str, str]:
+        # transforma uma str num dict
+        if not metadata_str:
+            return {}
+        meta_dict: Dict[str, str] = {}
+        for pair in metadata_str.split():
+            if '=' in pair:
+                key, value = pair.split('=', 1)
+                meta_dict[key] = value
+        return meta_dict
+    
 
+    def _parse_zone(self, base_line: str,
+                    metadata: Dict[str, str], line_num: int) -> None:
         
