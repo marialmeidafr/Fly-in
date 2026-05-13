88904ec6d3a0b4b3c79f0ca88543ecdea0fe9c34
Maria Paula Almeida De Freitas <mariaalm@c1r2s1.42porto.com>
2026-05-13 20:12:33 +0100

    change the maps

--- src/main.py
+++ src/main.py
@@ -8,3 +8,13 @@
 #else:
     # Modo terminal padrão (obrigatório)
  #   simulacao.run_text_mode()
+
+
+#for drone in all_drones:
+    # 1. Calcula o melhor caminho livre atual
+ #   path = pathfinder.find_path_with_reservations(start, end, 0)
+    
+    # 2. BLOQUEIA esses espaços para os drones seguintes
+  #  for zone_name, time in path:
+   #     pathfinder.add_reservation(zone_name, time)
+        # Adicionar também link_reservation entre os passos do path...
